a6c1a51c11f1a8d52473ca352ca16438ff876a37
Luca de Alfaro <luca@dealfaro.org>
2020-05-20 15:32:21 -0700

    Initial version of homework starter

---
+++ components/thumbrater.py
@@ -0,0 +1,9 @@
+from py4web import action, URL, request
+from yatl.helpers import XML
+from py4web.utils.url_signer import URLSigner
+from py4web.core import Fixture
+
+class ThumbRater(Fixture):
+
+    ### TODO: Complete.
+    pass
